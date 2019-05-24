4edba59942a583e1e8d00a1fc0a587bf780f6654
DiceMono <qrdfg@naver.com>
2019-05-24 12:00:52 +0900

    save method override

--- place/models.py
+++ place/models.py
@@ -21,3 +21,15 @@ class Place(models.Model):
     price = models.CharField(max_length=30, null=True)
     car = models.CharField(max_length=20, null=True)
     opening_hour = models.CharField(max_length=20, null=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.detail_category in ('카페 / 디저트', '베이커리'):
+            self.category = Category.objects.get(name='후식')
+
+        elif self.detail_category in ('일반 주점', '치킨 / 호프 / 펍', '칵테일 / 와인', '전통 주점 / 포차', '이자카야 / 오뎅 / 꼬치'):
+            self.category = Category.objects.get(name='술')
+
+        else:
+            self.category = Category.objects.get(name='밥')
+
+        return super().save(force_insert=False, force_update=False, using=None, update_fields=None)
