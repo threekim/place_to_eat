ba80770f1a40c4ae32a614e623f55baadb641147
DiceMono <qrdfg@naver.com>
2019-05-24 15:37:57 +0900

    add disqus

--- place/urls.py
+++ place/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import search_view, Detail_view, PlaceLike, PlaceFavorite
+from .views import search_view, detail_view, PlaceLike, PlaceFavorite
 
 app_name = 'place'
 
 urlpatterns = [
-    path('search/', search_view, name='search'),
-    path('detail/<int:place_id>/', Detail_view, name='detail'),
+    path('detail/<int:place_id>/', detail_view, name='detail'),
     path('like/<int:place_id>/', PlaceLike.as_view(), name ='like'),
     path('favorite/<int:place_id>/', PlaceFavorite.as_view(), name='favorite'),
+    path('', search_view, name='search'),
 ]
