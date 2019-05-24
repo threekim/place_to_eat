c72dd1b626c7f0e9e665aadfb905c1f5de2f3139
byeonguk kim <gang0406gang@gmail.com>
2019-05-24 12:59:28 +0900

    login & logout i did login & logout html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .forms import SignUpForm
+from django.views.generic.list import ListView
+from .models import Follow
+
+
+def signup(request):
+    if request.method == 'POST':
+        signup_form = SignUpForm(request.POST)
+
+        if signup_form.is_valid():
+            user_instance = signup_form.save(commit=False)
+            user_instance.set_password(signup_form.cleaned_data['password'])
+            user_instance.save()
+            return render(request, 'accounts/signup_complete.html', {'username':user_instance.username})
+
+    else:
+        signup_form = SignUpForm()
+
+    return render(request, 'accounts/signup.html', {'form':signup_form.as_p})
+
