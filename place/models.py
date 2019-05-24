0c8c6df17685bbc6cd3a4a4625943ed8a04e2d7c
DiceMono <qrdfg@naver.com>
2019-05-24 12:00:25 +0900

    set model base

--- place/models.py
+++ place/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Place(models.Model):
+    area = models.ManyToManyField(Area, related_name='places')
+    name = models.CharField(max_length=30)
+    url = models.URLField()
+    image_url = models.URLField(null=True)
+    address = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='places')
+    detail_category = models.CharField(max_length=30, null=True)
+    price = models.CharField(max_length=30, null=True)
+    car = models.CharField(max_length=20, null=True)
+    opening_hour = models.CharField(max_length=20, null=True)
